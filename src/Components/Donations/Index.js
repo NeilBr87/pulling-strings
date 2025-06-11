94ab4b74fd9489cf9cff985db1efaa49d0126056
Neil Brooks <neil.brooks87@gmail.com>
2025-06-11 20:34:54 +0100

    Donations by party leader

--- src/Components/Donations/Index.js
+++ src/Components/Donations/Index.js
@@ -21,7 +21,18 @@ export default function Donations() {
             <p>Description: to the central Labour Party to cover staffing costs for my frontbench role, paid in an initial instalment of £20,200 on 15 June 2023, then £19,400 a month thereafter until May 2024. This arrangement is expected to continue until the general election; the value above covers the first twelve months only.</p>
             </div>
 
-
+            <div className="infoContainer">
+            <h4>Donations by party leader</h4>
+            <p>Kemi Badenoch (Conservative): £623,674.95</p>
+            <p>Carla Denyer & Adrian Ramsay (Greens): £410,640.27</p>
+            <p>Ed Davey (Liberal Democrats): £75,875</p>
+            <p>Kier Starmer (Labour): £0 </p>
+            <p>Nigel Farage (Reform): £0 </p>
+            <p>John Swinney (Scottish National Party): £0 </p>
+            <p>Rhun ap Iorwerth (Plaid Cymru): £0 </p>
+            <p>Mary Lou McDonald (Sinn Fein): £0</p>
+            <p>Gavin Robinson (Democratic Unionist Party): £0</p>
+            </div>
 
         </div>
     )
