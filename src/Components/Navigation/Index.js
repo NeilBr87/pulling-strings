6b754e45ea43c027983f4fd1ed64c1b99ae962fc
Neil Brooks <neil.brooks87@gmail.com>
2025-06-15 18:01:25 +0100

    Style changes

--- src/Components/Navigation/Index.js
+++ src/Components/Navigation/Index.js
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import './style.css'
 import logo from './logo.png'
 import KnowledgeBase from '../KnowledgeBase/Index.js'
+import Voting from '../Votes/Index.js'
 import InterestContainer from '../InterestContainer/Index.js'
 export default function Navigation() {
 
    const [knowledgeBase, setKnowledgeBase] = useState(false)
     const [contact, setContact] = useState(false)
+    const [voting, setVoting] = useState(false)
    function openKB() {
     setKnowledgeBase(true)
    }
@@ -15,10 +17,14 @@ export default function Navigation() {
     setContact(!contact)
    }
 
+   function openVoting() {
+    setVoting(true)
+   }
+
     return (
         <div>
 
-            {!knowledgeBase && 
+            {!knowledgeBase && !voting && 
             <div id="navPage">
                 <div style={{backgroundColor: '#DBDBDB'}}>
                 <img src={logo} alt="logo" style={{width: '30vw', height: '10vh'}} />
@@ -28,7 +34,8 @@ export default function Navigation() {
                 <div id="pageImage"><p>"Money in politics is like water running downhill -- it finds its way."</p><p>- Jonathan Alter</p></div>
                 <h2>Menu</h2>
                 <InterestContainer />
-                <button id="voteButton">Commons vote tracker</button>
+                <button id="voteButton" onClick={openVoting}>Commons vote tracker</button>
+                <button id="lordsButton">Lords Voting Tracker</button>
                 <button id="kbButton" onClick={openKB}>Knowledge Base</button>
                 <button id="contactButton" onClick={openContact}>Contact</button>
                 {contact && <div style={{marginTop: '4vh'}}>Feel free to get in contact with me via my <a style={{  textDecoration: 'none', fontWeight: 'bold'}} href="https://neil-brooks-portfolio.netlify.app/portfoliopage">Portfolio</a></div>}
@@ -36,6 +43,8 @@ export default function Navigation() {
             </div>}
 
             {knowledgeBase && <KnowledgeBase knowledgeBase={knowledgeBase} setKnowledgeBase={setKnowledgeBase} />}
+            {voting && <Voting knowledgeBase={knowledgeBase} setKnowledgeBase={setKnowledgeBase} />}
+            
 
         </div>
     )
